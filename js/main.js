953691d65c2f64f924cf6835856561bcdb0dcfb6
Evan Dorsky <evan.dorsky@icloud.com>
2015-05-22 19:45:46 -0400

    Charge, field, and energy!

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ var Q = _.generate(function charge(x) {
   var pos =  2*x/(l-1) - 1;
   var apos = Math.abs(pos);
   if (apos < .2)
-    return pos == 0? 0 : apos/pos; // sign of x
+    return pos == 0? 0 : -apos/pos; // sign of x
 
   return 0;
 }, l);
@@ -24,28 +24,33 @@ var efield = integrate(Q);
 
 var E = integrate(efield);
 
-window.onload = function() {
-  var Energy = E.toArray();
+function makeplot(w, h) {
+  return d3.select('body')
+  .append('svg').attr('width', w).attr('height', h);
+}
 
-  var w = 600;
-  var h = 600;
-
+function plot(svg, data, subx, suby, yoff) {
   var x = d3.scale.linear()
-    .domain([0, Energy.length])
-    .range([0, w]);
+    .domain([0, data.length])
+    .range([0, subx]);
 
   var y = d3.scale.linear()
-    .domain(d3.extent(Energy))
-    .range([0, h]);
+    .domain(d3.extent(data))
+    .range([yoff, yoff+suby]);
 
   var eline = d3.svg.line()
     .x(function(d, i) { return x(i) })
     .y(function(d) { return y(d) })
 
-  d3.select('body')
-  .append('svg').attr('width', w).attr('height', h)
-  .append('g')
+  svg.append('g')
   .append('path')
   .attr('class', 'line')
-  .attr('d', eline(Energy))
+  .attr('d', eline(data))
+}
+
+window.onload = function() {
+  var svg = makeplot(600, 600);
+  plot(svg, Q.toArray(), 600, 200, 0);
+  plot(svg, efield.toArray(), 600, 200, 200);
+  plot(svg, E.toArray(), 600, 200, 400);
 }
