4449fe09051c252799c5a11b9a0865a887699f05
Evan Dorsky <evan.dorsky@icloud.com>
2015-05-22 16:15:43 -0400

    Drawing things

--- js/main.js
+++ js/main.js
@@ -28,5 +28,15 @@ var charge = _.generate(function charge(x) {
 var efield = integrate(charge, l);
 
 var E = integrate(efield, l);
-console.log('E');
-console.log(E);
+
+$(document).ready(function() {
+  d3.select('body')
+  .append('svg').attr('width', 600).attr('height', 600)
+  .append('g')
+  .selectAll('circle')
+  .data(E.toArray())
+  .enter().append('circle')
+  .attr('cx', function(d, i) {return i*3})
+  .attr('cy', function(d) {return d+600})
+  .attr('r', 1)
+})
