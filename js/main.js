0ccbe6eeb7bd007fcf27dd93cf89d05cedbf81bd
Evan Dorsky <evan.dorsky@icloud.com>
2015-05-22 13:27:04 -0400

    Cumulative sum works

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
 (function() {
-  alert('d3');
-  alert(d3);
+  _ = Lazy;
+
+  // integrate f from a to be with n even
+  function integrate(f, a, b, n) {
+    var h = (b - a)/n;
+    var s = f(a) + f(b);
+
+    // odd numbers
+    s += _.range(1, n, 2)
+      .map(function(i) { return 4*f(a + i*h) })
+      .reduce(function(x, y) { return x + y });
+
+    // even numbers
+    s += _.range(2, n-1, 2)
+      .map(function(i) { return 2*f(a + i*h) })
+      .reduce(function(x, y) { return x + y });
+
+    return s * h/3;
+  }
+
+  var cumulative = 0;
+  var time = _.range(100)
+  .map(function(i, x) {
+    cumulative += x;
+    return cumulative  
+  })
+  .toArray();
+
+  console.log('time');
+  console.log(time);
 })()
