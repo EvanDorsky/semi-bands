2737cc1e2edaeb8d27447164c00df5aa723b4a09
Evan Dorsky <evan.dorsky@icloud.com>
2015-05-23 20:33:11 -0700

    Multiple-scale sequence

--- js/main.js
+++ js/main.js
@@ -5,8 +5,11 @@ function integrate(f) {
   var l = f.size();
   var acc = 0;
   return _.generate(function(i) {
-      acc += f.get(i)/l;
-      return acc;
+      acc += f.get(i).y/l;
+      return {
+        x: f.get(i).x,
+        y: acc
+      };
     }, l).memoize();
 }
 
@@ -27,22 +30,36 @@ var xn = W*NA/(NA + ND);
 
 var L = .02;
 
-var l = 500;
-var rho = _.generate(function charge(x) {
-  // convert from step [0 - 499] to position in cm [-.01 - .01]
-  var pos =  x/(l-1)*L - L/2;
+var lL = 100;
+var lR = 100;
+var rho = _.range(-L/2, 0, L/(2*lL))
+  .concat(_.range(0, L/2, L/(2*lR)))
+  .map(function charge(x) {
+    if (-xp < x && x < 0)
+      return {
+        x: x,
+        y: -q*NA
+      };
 
-  if (-xp < pos && pos < 0)
-    return -q*NA;
+    if (0 < x && x < xn)
+      return {
+        x: x,
+        y: q*ND
+      };
 
-  if (0 < pos && pos < xn)
-    return q*ND;
-
-  return 0;
-}, l);
+    return {
+      x: x,
+      y: 0
+    };
+  }).memoize();
 
 var efield = integrate(rho)
-  .map(function(n) {return q*n/(kS*e0)});
+  .map(function(n) {
+    return {
+      x: n.x,
+      y: q*n.y/(kS*e0)
+    }
+  });
 
 var E = integrate(efield);
 
@@ -53,27 +70,27 @@ function makeplot(w, h) {
 
 function plot(svg, label, data, numx, numy, ypos) {
   var x = d3.scale.linear()
-    .domain([0, data.length])
+    .domain(d3.extent(data.pluck('x').toArray()))
     .range([0, svg.attr('width')/numx]);
 
   var h = svg.attr('height')/numy;
   var y = d3.scale.linear()
-    .domain(d3.extent(data))
+    .domain(d3.extent(data.pluck('y').toArray()))
     .range([ypos*h, (ypos+1)*h]);
 
   var eline = d3.svg.line()
-    .x(function(d, i) { return x(i) })
-    .y(function(d) { return y(d) })
+    .x(function(d) { return x(d.x) })
+    .y(function(d) { return y(d.y) })
 
   svg.append('g')
   .append('path')
   .attr('class', 'line '+label)
-  .attr('d', eline(data))
+  .attr('d', eline(data.toArray()))
 }
 
 window.onload = function() {
   var svg = makeplot(600, 600);
-  plot(svg, 'charge', rho.toArray(), 1, 3, 0);
-  plot(svg, 'field', efield.toArray(), 1, 3, 1);
-  plot(svg, 'energy', E.toArray(), 1, 3, 2);
+  plot(svg, 'charge', rho, 1, 3, 0);
+  plot(svg, 'field', efield, 1, 3, 1);
+  plot(svg, 'energy', E, 1, 3, 2);
 }
