556dc2e33fad69755d596c3230ed93bb5b3efb08
Evan Dorsky <evan.dorsky@icloud.com>
2015-05-24 18:30:47 -0700

    Much nicer code organization

--- js/main.js
+++ js/main.js
@@ -26,92 +26,118 @@ var T = 300; // K
 var k = 1.381e-23; // J/K
 var ni = 1e10; // 1/cm^3 | p*n=ni^2
 
-function pn(NA, ND, VA, L) {
-  var dep = depletion(NA, ND, VA);
-
-  var rho = _.range(-L/2, 0, L/2*dep.xp)
-    .concat(_.range(0, L/2, L/2*dep.xn))
-    .map(function charge(x) {
-      if (-dep.xp < x && x < 0)
-        return { x: x,y: -q*NA };
-
-      if (0 < x && x < dep.xn)
-        return { x: x, y: q*ND };
-
-      return { x: x, y: 0 };
-    }).memoize();
-
-  var efield = integrate(rho)
-    .map(function(n) {
-      return { x: n.x, y: q*n.y/(kS*e0) }
-    });
-
-  var E = integrate(efield).map(function(d) {return {x:d.x, y:-d.y/q}});
-
-  return {
-    rho: rho,
-    efield: efield,
-    E: E,
-    Vbi: dep.V0
-  }
-}
-
-function depletion(NA, ND, VA) {
-  var V0 = k*T/q*Math.log((NA*ND)/(ni*ni)); // V
-  var W = Math.sqrt(2*kS*e0/q*(1/NA + 1/ND)*(V0 - VA)); // cm?
-  var xp = W*ND/(NA + ND);
-  var xn = W*NA/(NA + ND);
-  return {
-    V0: V0,
-    xp: xp,
-    xn: xn
-  }
+function updatepnPlot(newpn, rhoC, efieldC, EC) {
+  d3.select('#rho')
+  .datum(newpn.rho)
+  .call(rhoC.update)
+  
+  d3.select('#efield')
+  .datum(newpn.efield)
+  .call(efieldC.update)
+  
+  d3.select('#E')
+  .datum(newpn.E)
+  .call(EC.update)
 }
 
 function plotpn(pnjunc) {
   var rhoC = pnChart();
-
   d3.select('body')
     .append('div')
     .attr('id', 'rho')
     .datum(pnjunc.rho)
-    .call(rhoC);
+    .call(rhoC)
+    .call(rhoC.update)
 
   var efieldC = pnChart();
-
   d3.select('body')
     .append('div')
     .attr('id', 'efield')
     .datum(pnjunc.efield)
     .call(efieldC)
+    .call(efieldC.update)
 
   var EC = pnChart();
-
   d3.select('body')
     .append('div')
     .attr('id', 'E')
     .datum(pnjunc.E)
     .call(EC)
+    .call(EC.update)
     .on('mousemove', function() {
       var V = EC.yScale().invert(d3.mouse(this)[1]-EC.margin().bottom)
       if (V<.05)
         return
 
-      var newpn = pn(NA, ND, pnjunc.Vbi-V, L)
-
-      d3.select('#rho')
-      .datum(newpn.rho)
-      .call(rhoC.update)
-      
-      d3.select('#efield')
-      .datum(newpn.efield)
-      .call(efieldC.update)
-      
-      d3.select('#E')
-      .datum(newpn.E)
-      .call(EC.update)
+      pnjunc.VA(pnjunc.Vbi-V);
+      updatepnPlot(pnjunc, rhoC, efieldC, EC)
     })
+}
 
+function pnJunction(props) {
+  var depletion = function dep(NA, ND, VA) {
+    var V0 = k*T/q*Math.log((NA*ND)/(ni*ni)); // V
+    var W = Math.sqrt(2*kS*e0/q*(1/NA + 1/ND)*(V0 - VA)); // cm?
+    var xp = W*ND/(NA + ND);
+    var xn = W*NA/(NA + ND);
+    return {
+      V0: V0,
+      xp: xp,
+      xn: xn
+    }
+  }
+
+  var junc = {
+    update: function pn() {
+      var NA = p.NA;
+      var ND = p.ND;
+      var VA = p.VA;
+      var L = p.L;
+
+      var dep = depletion(NA, ND, VA);
+      junc.Vbi = dep.V0;
+
+      junc.rho = _.range(-L/2, 0, L/2*dep.xp)
+        .concat(_.range(0, L/2, L/2*dep.xn))
+        .map(function charge(x) {
+          if (-dep.xp < x && x < 0)
+            return { x: x,y: -q*NA };
+
+          if (0 < x && x < dep.xn)
+            return { x: x, y: q*ND };
+
+          return { x: x, y: 0 };
+        }).memoize();
+
+      junc.efield = integrate(junc.rho)
+        .map(function(n) {
+          return { x: n.x, y: q*n.y/(kS*e0) }
+        });
+
+      junc.E = integrate(junc.efield).map(function(d) {return {x:d.x, y:-d.y/q}});
+    }
+  }
+
+  // defaults
+  var p = {
+    NA: 5e14,
+    ND: 1e14,
+    VA: 0,
+    L: 0.02
+  }
+
+  // generate getters/setters
+  Object.keys(p).forEach(function(key) {
+    junc[key] = function(_) {
+      if (!arguments.length) return junc;
+      p[key] = _;
+      junc.update();
+      return junc;
+    };
+  })
+
+  junc.update();
+  return junc;
 }
 
 function makeplot(w, h) {
@@ -120,12 +146,27 @@ function makeplot(w, h) {
 }
 
 window.onload = function() {
-  window.NA = 5e14; // 1/cm^3
-  window.ND = 1e14; // 1/cm^3
-  window.VA = 0;
-  window.L = 0.02;
+  var NA = 5e14; // 1/cm^3
+  var ND = 1e14; // 1/cm^3
+  var VA = 0;
+  var L = 0.02;
 
-  plotpn(pn(NA, ND, VA, L));
+  // init options currently ignored
+  var junction = new pnJunction({
+    NA: NA,
+    ND: ND,
+    VA: VA,
+    L: L
+  });
+
+  d3.select('button.NA')
+    .on('click', function() {
+      // var NAnew = parseInt(d3.select('input.NA')
+      //   .property('value'))
+      // updatepnPlot
+    })
+
+  plotpn(junction);
 }
 
 function pnChart() {
@@ -138,7 +179,6 @@ function pnChart() {
 
   function chart(selection) {
     selection.each(function(data) {
-
       xScale
         .domain(d3.extent(data.pluck('x').toArray()))
         .range([0, width - margin.left - margin.right]);
@@ -159,9 +199,6 @@ function pnChart() {
 
       var g = svg.select('g')
         .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-      g.select('.line')
-        .attr('d', line);
     });
   }
 
