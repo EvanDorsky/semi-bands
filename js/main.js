90a4256cfcfd4516cb7a79bd36e4ae98177ba074
Evan Dorsky <evan.dorsky@icloud.com>
2015-05-25 23:14:48 -0700

    Just... no more semicolons

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-_ = Lazy;
+_ = Lazy
 
 // representation of a polynomial function
 // coefs = [a0, a1, a2, a3, ...]
@@ -6,38 +6,38 @@ function Poly(_coefs) {
   var poly = {
     coefs: _coefs,
     diff: function() {
-      var dcoefs = poly.coefs;
+      var dcoefs = poly.coefs
       for (var i in dcoefs)
-        dcoefs[i] *= Number(i)+1;
+        dcoefs[i] *= Number(i)+1
 
       dcoefs.shift()
-      poly.coefs = dcoefs;
+      poly.coefs = dcoefs
 
-      return poly;
+      return poly
     },
     int: function(C) {
-      var icoefs = poly.coefs;
+      var icoefs = poly.coefs
       for (var i in icoefs)
-        icoefs[i] /= Number(i)+2;
+        icoefs[i] /= Number(i)+2
 
-      icoefs.unshift(C);
-      poly.coefs = icoefs;
+      icoefs.unshift(C)
+      poly.coefs = icoefs
 
-      return poly;
+      return poly
     },
     sampled: function(range, dx) {
-      var a = range[0];
-      var b = range[range.length-1];
+      var a = range[0]
+      var b = range[range.length-1]
       if (arguments.length === 2) {
-        var X = _.range(a, b, dx);
+        var X = _.range(a, b, dx)
 
         var Y = X.map(function(x) {
           return _(poly.coefs).map(function(coef, i) {
-            return coef*Math.pow(x, i);
+            return coef*Math.pow(x, i)
           }).memoize().reduce(function(x,y){
             return x+y
-          });
-        });
+          })
+        })
 
         return X.zip(Y.toArray()).map(function(x) {
           return {
@@ -49,14 +49,14 @@ function Poly(_coefs) {
     },
     sampledAt: function(x) {
       return _(poly.coefs).map(function(coef, i) {
-        return coef*Math.pow(x, i);
+        return coef*Math.pow(x, i)
       }).memoize().reduce(function(x,y){
         return x+y
-      });
+      })
     }
   }
 
-  return poly;
+  return poly
 }
 
 function PolyFunc(_polys) {
@@ -66,18 +66,18 @@ function PolyFunc(_polys) {
       var temp = [{
         poly: func.polys[0].poly.int(0),
         range: func.polys[0].range
-      }];
+      }]
       for (var i = 1; i < func.polys.length; i++) {
-        var r = func.polys[i].range;
+        var r = func.polys[i].range
         temp.push({
           poly: func.polys[i].poly.int(temp[i-1].poly.sampledAt(r[0])),
           range: r
         })
       }
 
-      func.polys = temp;
+      func.polys = temp
 
-      return func;
+      return func
     },
     diff: function() {
       func.polys = func.polys.map(function(spec) {
@@ -87,7 +87,7 @@ function PolyFunc(_polys) {
         }
       })
       
-      return func;
+      return func
     },
     copy: function() {
       return new PolyFunc(func.polys)
@@ -99,23 +99,23 @@ function PolyFunc(_polys) {
     }
   }
 
-  return func;
+  return func
 }
 
 // physical constants
-var e0 = 8.8542e-12; // F/m
-var kS = 11.9;
-var q = 1.602e-19; // J
-var T = 300; // K
-var k = 1.381e-23; // J/K
-var ni = 1e10; // 1/cm^3 | p*n=ni^2
+var e0 = 8.8542e-12 // F/m
+var kS = 11.9
+var q = 1.602e-19 // J
+var T = 300 // K
+var k = 1.381e-23 // J/K
+var ni = 1e10 // 1/cm^3 | p*n=ni^2
 
 function pnJunction(props) {
   var depletion = function dep(NA, ND, VA) {
-    var V0 = k*T/q*Math.log((NA*ND)/(ni*ni)); // V
-    var W = Math.sqrt(2*kS*e0/q*(1/NA + 1/ND)*(V0 - VA)); // cm?
-    var xp = W*ND/(NA + ND);
-    var xn = W*NA/(NA + ND);
+    var V0 = k*T/q*Math.log((NA*ND)/(ni*ni)) // V
+    var W = Math.sqrt(2*kS*e0/q*(1/NA + 1/ND)*(V0 - VA)) // cm?
+    var xp = W*ND/(NA + ND)
+    var xn = W*NA/(NA + ND)
     return {
       V0: V0,
       xp: xp,
@@ -125,13 +125,13 @@ function pnJunction(props) {
 
   var junc = {
     update: function pn() {
-      var NA = p.NA;
-      var ND = p.ND;
-      var VA = p.VA;
-      var L = p.L;
+      var NA = p.NA
+      var ND = p.ND
+      var VA = p.VA
+      var L = p.L
 
-      var dep = depletion(NA, ND, VA);
-      junc.Vbi = dep.V0;
+      var dep = depletion(NA, ND, VA)
+      junc.Vbi = dep.V0
 
       var rho = new PolyFunc([
         {
@@ -169,21 +169,21 @@ function pnJunction(props) {
 
   if (arguments.length)
     Object.keys(props).forEach(function(key) {
-      p[key] = props[key];
-    });
+      p[key] = props[key]
+    })
 
   // generate getters/setters
   Object.keys(p).forEach(function(key) {
     junc[key] = function(_) {
-      if (!arguments.length) return p[key];
-      p[key] = _;
-      junc.update();
-      return junc;
-    };
+      if (!arguments.length) return p[key]
+      p[key] = _
+      junc.update()
+      return junc
+    }
   })
 
-  junc.update();
-  return junc;
+  junc.update()
+  return junc
 }
 
 window.onload = function() {
@@ -192,32 +192,32 @@ window.onload = function() {
     ND: 4e14,
     VA: 0,
     L: .02
-  });
+  })
 
-  var PNC = pnChart(junction);
+  var PNC = pnChart(junction)
 
   d3.select('button.NA')
     .on('click', function() {
       var NAnew = Number(d3.select('input.NA')
-        .property('value'));
+        .property('value'))
       PNC.update({
         NA: NAnew
-      });
+      })
     })
 
   d3.select('button.ND')
     .on('click', function() {
       var NDnew = Number(d3.select('input.ND')
-        .property('value'));
+        .property('value'))
       PNC.update({
         ND: NDnew
-      });
+      })
     })
 }
 
 function pnChart(junc) {
   if (!arguments.length)
-    console.error('Requires a pn junction!');
+    console.error('Requires a pn junction!')
   var chart = {
     junc: junc,
     rhoC: semiChart(),
@@ -226,7 +226,7 @@ function pnChart(junc) {
     update: function updatepnChart(opts) {
       Object.keys(opts).forEach(function(key) {
         chart.junc[key](opts[key])
-      });
+      })
 
       d3.select('#rho')
       .datum(chart.junc.rho)
@@ -240,7 +240,7 @@ function pnChart(junc) {
       .datum(chart.junc.E)
       .call(chart.EC.update)
     }
-  };
+  }
 
   chart.rhoC.line().interpolate('step')
   d3.select('body')
@@ -248,14 +248,14 @@ function pnChart(junc) {
     .attr('id', 'rho')
     .datum(chart.junc.rho)
     .call(chart.rhoC)
-    .call(chart.rhoC.update);
+    .call(chart.rhoC.update)
 
   d3.select('body')
     .append('div')
     .attr('id', 'efield')
     .datum(chart.junc.efield)
     .call(chart.efieldC)
-    .call(chart.efieldC.update);
+    .call(chart.efieldC.update)
 
   d3.select('body')
     .append('div')
@@ -270,8 +270,8 @@ function pnChart(junc) {
 
       chart.update({
         VA: chart.junc.Vbi-V
-      });
-    });
+      })
+    })
 
   return chart
 }
@@ -282,24 +282,24 @@ function semiChart() {
     height = 120,
     xScale = d3.scale.linear(),
     yScale = d3.scale.linear(),
-    line = d3.svg.line().x(X).y(Y);
+    line = d3.svg.line().x(X).y(Y)
 
   function chart(selection) {
     selection.each(function(data) {
       xScale
         .domain(d3.extent(data.pluck('x').toArray()))
-        .range([0, width - margin.left - margin.right]);
+        .range([0, width - margin.left - margin.right])
 
       yScale
         .domain(d3.extent(data.pluck('y').toArray()))
-        .range([height - margin.top - margin.bottom, 0]);
+        .range([height - margin.top - margin.bottom, 0])
 
       // Select the svg element, if it exists.
-      var svg = d3.select(this).selectAll('svg').data([data.toArray()]);
+      var svg = d3.select(this).selectAll('svg').data([data.toArray()])
 
       // Otherwise, create the skeletal chart.
-      var gEnter = svg.enter().append('svg').append('g');
-      // gEnter.append('path').attr('class', 'line');
+      var gEnter = svg.enter().append('svg').append('g')
+      // gEnter.append('path').attr('class', 'line')
       svg.select('g').selectAll('circle')
       .data(data.toArray())
       .enter().append('circle')
@@ -308,21 +308,21 @@ function semiChart() {
       .attr('r', 2)
 
       svg.attr('width', width)
-         .attr('height', height);
+         .attr('height', height)
 
       var g = svg.select('g')
-        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-    });
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+    })
   }
 
   chart.update = function(selection) {
     selection.each(function(data) {
 
-      // var svg = d3.select(this).selectAll('svg').data([data.toArray()]);
-      var svg = d3.select(this).selectAll('svg');
+      // var svg = d3.select(this).selectAll('svg').data([data.toArray()])
+      var svg = d3.select(this).selectAll('svg')
       // svg.select('g').select('.line')
       //   .attr('d', line)
-      // });
+      // })
       var upd = svg.select('g').selectAll('circle')
       .data(data.toArray())
 
@@ -334,59 +334,59 @@ function semiChart() {
 
       upd.exit().remove()
 
-      return chart;
+      return chart
     })
   }
 
   function X(d) {
-    return xScale(d.x);
+    return xScale(d.x)
   }
 
   function Y(d) {
-    return yScale(d.y);
+    return yScale(d.y)
   }
 
   chart.margin = function(_) {
-    if (!arguments.length) return margin;
-    margin = _;
-    return chart;
-  };
+    if (!arguments.length) return margin
+    margin = _
+    return chart
+  }
 
   chart.line = function(_) {
-    if (!arguments.length) return line;
-    line = _;
-    return chart;
-  };
+    if (!arguments.length) return line
+    line = _
+    return chart
+  }
 
   chart.width = function(_) {
-    if (!arguments.length) return width;
-    width = _;
-    return chart;
-  };
+    if (!arguments.length) return width
+    width = _
+    return chart
+  }
 
   chart.yScale = function(_) {
-    if (!arguments.length) return yScale;
-    yScale = _;
-    return chart;
-  };
+    if (!arguments.length) return yScale
+    yScale = _
+    return chart
+  }
 
   chart.xScale = function(_) {
-    if (!arguments.length) return xScale;
-    xScale = _;
-    return chart;
-  };
+    if (!arguments.length) return xScale
+    xScale = _
+    return chart
+  }
 
   chart.margin = function(_) {
-    if (!arguments.length) return margin;
-    margin = _;
-    return chart;
-  };
+    if (!arguments.length) return margin
+    margin = _
+    return chart
+  }
 
   chart.height = function(_) {
-    if (!arguments.length) return height;
-    height = _;
-    return chart;
-  };
+    if (!arguments.length) return height
+    height = _
+    return chart
+  }
 
-  return chart;
+  return chart
 } // after http://bost.ocks.org/mike/chart/
