9985d3322df194e44572dc1896180940b81ed17d
Evan Dorsky <evan.dorsky@icloud.com>
2015-05-26 10:39:07 -0700

    Surprisingly, math works!

--- js/test.js
+++ js/test.js
@@ -111,185 +111,53 @@ function PolyFunc(_polys) {
   return func
 }
 
-// physical constants
-var e0 = 8.8542e-12 // F/m
-var kS = 11.9
-var q = 1.602e-19 // J
-var T = 300 // K
-var k = 1.381e-23 // J/K
-var ni = 1e10 // 1/cm^3 | p*n=ni^2
-
-function pnJunction(props) {
-  var depletion = function dep(NA, ND, VA) {
-    var V0 = k*T/q*Math.log((NA*ND)/(ni*ni)) // V
-    var W = Math.sqrt(2*kS*e0/q*(1/NA + 1/ND)*(V0 - VA)) // cm?
-    var xp = W*ND/(NA + ND)
-    var xn = W*NA/(NA + ND)
-    return {
-      V0: V0,
-      xp: xp,
-      xn: xn
-    }
-  }
-
-  var junc = {
-    update: function pn() {
-      var NA = p.NA
-      var ND = p.ND
-      var VA = p.VA
-      var L = p.L
-
-      var dep = depletion(NA, ND, VA)
-      junc.Vbi = dep.V0
-
-      var rho = new PolyFunc([
-        {
-          poly: Poly([0]),
-          range: [-L/2, -dep.xp]
-        },
-        {
-          poly: Poly([-q*NA]),
-          range: [-dep.xp, 0]
-        },
-        {
-          poly: Poly([q*ND]),
-          range: [0, dep.xn]
-        },
-        {
-          poly: Poly([0]),
-          range: [dep.xn, L/2]
-        }
-      ])
-
-      junc.rho = _(rho.sampled(p.dx))
-      junc.efield = _(rho.int(0).sampled(p.dx))
-      junc.E = _(rho.int(0).sampled(p.dx))
-    }
-  }
-
-  // defaults
-  var p = {
-    NA: 5e14,
-    ND: 1e14,
-    VA: 0,
-    L: 0.02,
-    dx: .0001
-  }
-
-  if (arguments.length)
-    Object.keys(props).forEach(function(key) {
-      p[key] = props[key]
-    })
-
-  // generate getters/setters
-  Object.keys(p).forEach(function(key) {
-    junc[key] = function(_) {
-      if (!arguments.length) return p[key]
-      p[key] = _
-      junc.update()
-      return junc
-    }
-  })
-
-  junc.update()
-  return junc
-}
-
 window.onload = function() {
-  var junction = new pnJunction({
-    NA: 1e14,
-    ND: 4e14,
-    VA: 0,
-    L: .02
-  })
-
-  var PNC = pnChart(junction)
-
-  d3.select('button.NA')
-    .on('click', function() {
-      var NAnew = Number(d3.select('input.NA')
-        .property('value'))
-      PNC.update({
-        NA: NAnew
-      })
-    })
-
-  d3.select('button.ND')
-    .on('click', function() {
-      var NDnew = Number(d3.select('input.ND')
-        .property('value'))
-      PNC.update({
-        ND: NDnew
-      })
-    })
-}
-
-function pnChart(junc) {
-  if (!arguments.length)
-    console.error('Requires a pn junction!')
-  var chart = {
-    junc: junc,
-    rhoC: semiChart(),
-    efieldC: semiChart(),
-    EC: semiChart(),
-    update: function updatepnChart(opts) {
-      Object.keys(opts).forEach(function(key) {
-        chart.junc[key](opts[key])
-      })
-
-      d3.select('#rho')
-      .datum(chart.junc.rho)
-      .call(chart.rhoC.update)
-      
-      d3.select('#efield')
-      .datum(chart.junc.efield)
-      .call(chart.efieldC.update)
-      
-      d3.select('#E')
-      .datum(chart.junc.E)
-      .call(chart.EC.update)
+  var PFtest = new PolyFunc([
+    {
+      poly: Poly([0]),
+      range: [-2, -1]
+    },
+    {
+      poly: Poly([-1]),
+      range: [-1, 0]
+    },
+    {
+      poly: Poly([1]),
+      range: [0, 1]
+    },
+    {
+      poly: Poly([0]),
+      range: [1, 2]
     }
-  }
+  ])
 
-  chart.rhoC.line().interpolate('step')
+  var Psampled = _(PFtest.sampled(.1))
+  console.log('Psampled');
+  console.log(Psampled.pluck('y').toArray());
+
+  var chart = semiChart()
   d3.select('body')
     .append('div')
-    .attr('id', 'rho')
-    .datum(chart.junc.rho)
-    .call(chart.rhoC)
-    .call(chart.rhoC.update)
+    .datum(Psampled)
+    .call(chart)
 
+  var Pintsampled = _(PFtest.int().sampled(.1))
+  console.log('Pintsampled');
+  console.log(Pintsampled.pluck('y').toArray());
+  console.log(_(PFtest.polys).pluck('poly').pluck('coefs').toArray());
+
+  var chart2 = semiChart()
   d3.select('body')
     .append('div')
-    .attr('id', 'efield')
-    .datum(chart.junc.efield)
-    .call(chart.efieldC)
-    .call(chart.efieldC.update)
-
-  d3.select('body')
-    .append('div')
-    .attr('id', 'E')
-    .datum(chart.junc.E)
-    .call(chart.EC)
-    .call(chart.EC.update)
-    .on('mousemove', function() {
-      var V = chart.EC.yScale().invert(d3.mouse(this)[1]-chart.EC.margin().bottom)
-      if (V<.05)
-        return
-
-      chart.update({
-        VA: chart.junc.Vbi-V
-      })
-    })
-
-  return chart
+    .datum(Pintsampled)
+    .call(chart2)
 }
 
 function semiChart() {
   var p = {
     margin: {top: 20, right: 20, bottom: 20, left: 20},
-    width: 760,
-    height: 120,
+    width: 600,
+    height: 300,
     xScale: d3.scale.linear(),
     yScale: d3.scale.linear(),
     line: d3.svg.line().x(X).y(Y)
@@ -298,11 +166,11 @@ function semiChart() {
   function chart(selection) {
     selection.each(function(data) {
       p.xScale
-        .domain(d3.extent(data.pluck('x').toArray()))
+        .domain([-2, 2])
         .range([0, p.width - p.margin.left - p.margin.right])
 
       p.yScale
-        .domain(d3.extent(data.pluck('y').toArray()))
+        .domain([-1, 1])
         .range([p.height - p.margin.top - p.margin.bottom, 0])
 
       // Select the svg element, if it exists.
